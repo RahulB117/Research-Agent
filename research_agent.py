f6369c99a9c84694b95e6bdb6562a3ed92a3005a
Rahul Babu <rahulbabu2nd1997@gmail.com>
2025-06-02 08:49:10 +0100

    FEAT: Add initial research agent, tools module and update .gitignore
    Signed-off-by: Rahul Babu <rahulbabu2nd1997@gmail.com>

---
+++ research_agent.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv #For API Key
+from pydantic import BaseModel # For validation of data
+from langchain_openai import ChatOpenAI # For using OpenAI's chat model
+from langchain_core.prompts import ChatPromptTemplate # For creating chat prompts
+from langchain_core.output_parsers import PydanticOutputParser #For parsing the output to a Pydantic model
+from langchain.agents import create_tool_calling_agent, AgentExecutor # For creating an agent that can call tools
+
+#Load environment variables from .env file
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+# Create a chat prompt template for the research agent
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         """
+         You are a research assistant tasked to acquire data on a topic to write a research paper. 
+         Answer the user query and use necessary tools.
+         I want the output in this format with no additional text\n{format_instructions}""",
+         ),
+        ("placeholder", "{chat_history}"), #chat_history is the conversation history
+        ("human","{query}"),
+        ("placeholder", "{agent_scratchpad}"), #agent_scratchpad is where the agent can write notes or thoughts
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools = []
+)
+# chat history and agent scratchpad are filled up by AgentExecutor
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[],
+    verbose=True)
+
+raw_response = agent_executor.invoke({"query": "What is the impact of climate change on polar bear populations?"})
+
+try:
+    # Attempt to parse the raw response using the Pydantic parser
+    structured_response = parser.parse(raw_response['output'])
+except Exception as e:
+    print(f"Error parsing response: {e}", "Raw Response = ", raw_response)
+
+print(structured_response)
